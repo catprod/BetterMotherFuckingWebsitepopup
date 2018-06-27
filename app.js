4c4d5ea0d9257a08f3695dabdd0e043b1e064a6a
catprod <ndrianarasoanaivo@gmail.com>
2018-06-27 11:06:58 +0200

    popup

---
+++ app.js
@@ -0,0 +1,10 @@
+function openModal(){
+    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}
+function openModale(){
+    var x= document.getElementById("body");
+    x.style.backgroundColor =  "magenta";
+    var y= document.getElementsByTagName("p");
+    y.style.color = "blue";
+    y.style.fontFamily = "papyrus";
+}
